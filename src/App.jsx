8b2614772b68b8957fd97b172498f7f04f1dc219
Chathura Kanchana <chathura.senavirathna@gmail.com>
2026-02-24 17:22:36 +0100

    Added Explore and Events sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Explore from "./components/Explore";
+import Events from "./components/Events";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Explore />
+      <Events />
     </>
   );
 }
