f438e1fb4497c87299f315e4f8d9e73d8b2aefa5
Chathura Kanchana <chathura.senavirathna@gmail.com>
2026-03-16 01:58:26 +0100

    Register GCS filesystem driver

--- uppgo-api/app/Providers/AppServiceProvider.php
+++ uppgo-api/app/Providers/AppServiceProvider.php
@@ -3,22 +3,34 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\Filesystem;
+use League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter;
+use Google\Cloud\Storage\StorageClient;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+
+        Storage::extend('gcs', function ($app, $config) {
+
+            $storageClient = new StorageClient([
+                'projectId' => $config['project_id'],
+            ]);
+
+            $bucket = $storageClient->bucket($config['bucket']);
+
+            $adapter = new GoogleCloudStorageAdapter($bucket);
+
+            return new Filesystem($adapter);
+
+        });
+
     }
 }
